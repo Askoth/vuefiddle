29443052b6c0e71006486a81376af3004bf3fdf5
Marcelo Oliveira <askoth@gmail.com>
2018-11-30 22:17:29 +0100

    Working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,58 @@
 new Vue({
-    el: '#app'
+    el: '#app',
+    data () {
+        return {
+            output: '',
+            tmpl: `&lt;div&gt;
+    {{ teste }}
+&lt;/div&gt;`,
+            js: `{
+    data () {
+        return {
+            teste: 1
+        }
+    }
+}`,
+            css: `body {
+    background: magenta
+}`,
+        }
+    },
+    methods: {
+        updateIframe() {
+            const { tmpl, js, css } = this.$refs;
+
+
+            this.output = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>${this.decodeHtml(css.textContent.replace(/\n/g, ''))}</style>
+</head>
+<body>
+    <div id="app"></div>
+
+    <script src="/js/vue.js"></script>
+    <script>
+        var a = new Vue({
+            template: '${this.decodeHtml(tmpl.textContent.replace(/\n/g, ''))}',
+            ...${this.decodeHtml(js.textContent.replace(/\n/g, ''))}
+        })
+
+        a.$mount('#app');
+    </script>
+</body>
+</html>
+`;
+
+        },
+        decodeHtml(html) {
+            var txt = document.createElement("textarea");
+            txt.innerHTML = html;
+            return txt.value;
+        }
+    }
 })
+
